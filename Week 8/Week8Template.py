d95d393e1716246f78dea848a6bb8470d1766215
samwincott <wincott97@gmail.com>
2016-11-21 12:38:54 +0000

    week 8 files added

---
+++ Week 8/Week8Template.py
@@ -0,0 +1,34 @@
+"""
+Does it work on files where no error checking is needed on the fields
+
+>>> sumRows("rows1.csv") == {'tim': 36.0, 'bob': 11.0, 'anna': 54.0}
+True
+
+Does it ignore headers if requested?
+
+>>> sumRows("rows1.csv", header=True) == {'tim': 36.0, 'anna': 54.0}
+True
+
+Is it returning the right type of result?
+>>> type(sumRows("rows1.csv"))
+<class 'dict'>
+
+Does it work on files with empty fields or fields which aren't numbers?
+
+>>> sumRows("rows2.csv") == {'tim': 24.0, 'bob': 11.0, 'anna': 13.0}
+True
+
+Does it sum columns correctly?
+>>> sumColumns("columns.csv") == {'': 0, 'tim': 5.0, 'bob': 41.0, 'anna': 55.0}
+True
+"""
+
+# *** DO NOT CHANGE CODE ABOVE THIS LINE ***
+# *** DO NOT ADD ANY COMMENTS OF YOUR OWN IN THE SUBMITTED SOLUTION ***
+
+def sumRows(filename, header=False):
+    # Add code here
+
+def sumColumns(filename):
+    # Add code here
+
