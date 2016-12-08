6e957c2a9ac10abece9448f86d4e3889751355ad
samwincott <wincott97@gmail.com>
2016-12-08 16:55:38 +0000

    done, maybe

--- coursework/main.py
+++ coursework/main.py
@@ -3,18 +3,15 @@ import csv
 import itertools 
 import matplotlib.pyplot as plt
 
-### I would have spaces in the functions to make the code more readable, but it was creating loads of indentation errors when transferring to an interactive shell
-
 def game(ra, rb):
 	p = ra / (ra + rb)
-	score_a = 0 
-	score_b = 0
-	game_over = False
+	score_a = score_b = 0
+	game_over = False	
 	while game_over == False:			
 		r = random.uniform(0, 1)
-		if r < p:
+		if r < p: 
 			score_a += 1
-		else:
+		else: 
 			score_b += 1
 		reached_eleven = (score_a >= 11) or (score_b >= 11)
 		two_point_difference = abs(score_a-score_b) > 1
@@ -22,18 +19,15 @@ def game(ra, rb):
 	return score_a, score_b
 
 def winProbability(ra, rb, n):
-	wins_a = 0
-	wins_b = 0
+	wins_a = wins_b = rallies = 0
 	# rallies is initialised for question 2
-	rallies = 0
 	for match in range(0, n):
 		score_a, score_b = game(ra, rb)
 		rallies += (score_a + score_b)
-		if score_a > score_b:
+		if score_a > score_b: 
 			wins_a += 1
-		else:
+		else: 
 			wins_b += 1 		
-	# the probability that A wins
 	pa = wins_a / (wins_a + wins_b) 
 	pa = round(pa, 2)
 	rallies = rallies / n
@@ -41,16 +35,14 @@ def winProbability(ra, rb, n):
 	return pa, rallies
 
 def englishWinProbability(ra, rb, n):
-	wins_a = 0
-	wins_b = 0
-	rallies = 0
+	wins_a = wins_b = rallies = 0
 	for match in range(0, n):
-		score_a, score_b, temp = english_game(ra, rb)
+		score_a, score_b, temp_rallies = english_game(ra, rb)
 		if score_a > score_b:
 			wins_a += 1
 		else:
 			wins_b += 1
-		rallies += temp     	
+		rallies += temp_rallies     	
 	# the probability that A wins
 	pa = wins_a / (wins_a + wins_b) 
 	pa = round(pa, 2)
@@ -61,6 +53,7 @@ def readCSV(file):
 	with open(file) as csvfile:
 		rdr = csv.reader(csvfile)
 		lot = []
+		# This is to ignore the header row
 		next(rdr)
 		for row in rdr:
 			lot.append((int(row[0]), int(row[1])))
@@ -72,20 +65,19 @@ def graphMaker(player_list):
 	rarb = []
 	for key in player_list:
 		pa.append(winProbability(key[0], key[1], 10000))
-		rarb.append(key[0] / key[1])    
+		rarb.append(key[0] / key[1])	    
 	plt.plot(rarb, pa)
 	plt.axis([0, 3.5, 0, 1])
 	plt.ylabel('Probability of A winning')
 	plt.xlabel('Player A ability / Player B ability')
+	plt.title('A graph to show the probability of player A winning, against the relative abilities of the two players')
 	plt.show()
 
 def english_game(ra, rb):
 	p = ra / (ra + rb)
 	server = None 
-	score_a = 0 
-	score_b = 0
+	score_a = score_b = rallies = 0 
 	play_to = 9
-	rallies = 0
 	game_over = False
 	while game_over == False:			
 		r = random.uniform(0, 1)
@@ -109,80 +101,77 @@ def english_game(ra, rb):
 
 def q2():
 	playerlist = readCSV('players.csv')
-	eng_pa_list = []
 	eng_rallies_list = []
 	rarb = []
-	pars_pa_list = []
 	pars_rallies_list = []
 	for key in playerlist:
-		eng_pa, eng_rallies = englishWinProbability(key[0], key[1], 10000)
-		eng_pa_list.append(eng_pa)
+		eng_rallies = englishWinProbability(key[0], key[1], 10000)[1]
 		eng_rallies_list.append(eng_rallies)
-		pars_pa, pars_rallies = winProbability(key[0], key[1], 10000)
-		pars_pa_list.append(pars_pa)
+		pars_rallies = winProbability(key[0], key[1], 10000)[1]
 		pars_rallies_list.append(pars_rallies)
 		rarb.append(key[0] / key[1])
-	plt.plot(rarb, eng_rallies_list)
-	plt.plot(rarb, pars_rallies_list)
+	eng_plot, = plt.plot(rarb, eng_rallies_list)
+	pars_plot, = plt.plot(rarb, pars_rallies_list)
 	plt.axis([0, 10, 10, 30])
 	plt.ylabel('Average time for game')
 	plt.xlabel('Relative abilities Ra/Rb')
 	plt.title('A graph showing relative abilities against time for rallies, for the English and PARS scoring systems')
+	plt.legend([eng_plot, pars_plot], ["English Scoring System", "PARS Scoring System"])
 	plt.show()
 
-def q1e(ra, rb, minProbability):
+def q1e(ra, rb, min_probability):
 	probabilities = {}
 	probabilities['W'] = winProbability(ra, rb, 1000000)[0]
 	probabilities['L'] = 1 - probabilities['W']
-	totalwinprobability = 0
-	numberOfGames = 0
-	while totalwinprobability < minProbability:
-		numberOfGames += 1	
+	total_win_probability = 0
+	number_of_games = 0
+	while total_win_probability < min_probability:
+		number_of_games += 1	
 		# these variables need to be reset each time
-		totalwinprobability = 0
-		listOfWinningCombinations = []
-		shortenedListOfCombinations = []
-		finalList = []
-		combinations = itertools.product('WL', repeat=((2*numberOfGames)-1))
+		total_win_probability = 0
+		list_of_winning_combinations = []
+		shortened_list_of_combinations = []
+		final_list = []
+		combinations = itertools.product('WL', repeat=((2*number_of_games)-1))
 		for comb in combinations:
 			wins = 0
 			for j in range(0, len(comb)):
 				if comb[j] == 'W':
 					wins += 1
-				if wins == numberOfGames:
-					listOfWinningCombinations.append(comb[:j+1])
-		for comb in listOfWinningCombinations:
+				if wins == number_of_games:
+					list_of_winning_combinations.append(comb[:j+1])
+		for comb in list_of_winning_combinations:
 			if comb.count('W') > comb.count('L'):
 				if comb[-1]  == 'W':
-					shortenedListOfCombinations.append(comb)
+					shortened_list_of_combinations.append(comb)
 			else:
 				if i[-1] == 'L':
-					shortenedListOfCombinations.append(comb)
+					shortened_list_of_combinations.append(comb)
 		# removing duplicates
-		finalList = set(shortenedListOfCombinations)
-		for comb in finalList:
-			winProb = 1
+		final_list = set(shortened_list_of_combinations)
+		for comb in final_list:
+			win_prob = 1
 			for char in comb:
-				winProb *= probabilities[char]  
-			if comb.count('W') == numberOfGames:
-				totalwinprobability += winProb          
-	print("To achieve a winning probability of " + str(minProbability) + " you'll need to play " + str(numberOfGames) + " games.")
+				win_prob *= probabilities[char]  
+			if comb.count('W') == number_of_games:
+				total_win_probability += win_prob          
+	print("To achieve a winning probability of " + str(min_probability) + " you will need to play " + str(number_of_games) + " games.")
 
-
-### For question 1a, uncomment the random seed in the 'game' function, and the following line:
+### For question 1a uncomment the following lines:
+# random.seed(57)
 # print(game(70, 30))   
 
-### For question 1b, uncomment the following line, and make sure the random seed in 'game' is not set:
+### For question 1b uncomment the following line:
 # print(winProbability(70, 30, 1000000)[0])
 
-### For question 1c, uncomment the following line:
+### For question 1c uncomment the following line:
 # print(readCSV('test.csv'))
 
-### For question 1d, make sure random seed is not set and uncomment the following line:
+### For question 1d uncomment the following line:
 # graphMaker(readCSV('test.csv'))
 
-### For question 1e, make sure random seed is not set and uncomment the following line:
+### For question 1e uncomment the following line:
 # q1e(60, 40, 0.9)
 
-### For question 2, make sure random seed is not set and uncomment the following line:
+### For question 2 uncomment the following line:
 # q2()
